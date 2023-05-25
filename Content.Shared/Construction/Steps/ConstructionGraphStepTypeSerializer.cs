2b3d425f20f2c01571272bd76626154caca94b73
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-05-25 06:29:12 -0400

    Initial push on Loudspeakers
    Adds functional loudspeakers to the game with a fleshed out construction graph. Adds FilterConstructionGraphStep.cs to facilitate this, allowing you to add entities lacing a list of components, tags, and under a certain size.
    Adds an audio for empty loudspeakers.

--- Content.Shared/Construction/Steps/ConstructionGraphStepTypeSerializer.cs
+++ Content.Shared/Construction/Steps/ConstructionGraphStepTypeSerializer.cs
@@ -3,6 +3,7 @@ using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Markdown.Mapping;
 using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
+using Content.Shared.SimpleStation14.Construction.Steps; // Parkstation-LoudSpeakers
 
 namespace Content.Shared.Construction.Steps
 {
@@ -45,6 +46,13 @@ namespace Content.Shared.Construction.Steps
             {
                 return typeof(PartAssemblyConstructionGraphStep);
             }
+
+            // Parkstation-LoudSpeakers-Start
+            if (node.Has("size") || node.Has("compBlacklist") || node.Has("tagBlacklist"))
+            {
+                return typeof(FilterConstructionGraphStep);
+            }
+            // Parkstation-LoudSpeakers-End
 
             return null;
         }
