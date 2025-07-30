f1b7f6e13b7701c74403839c4e66916f92b8b5f1
Pspritechologist <naaronn@gmail.com>
2025-07-30 04:17:18 -0400

    Line numbering! :D Super broke out the types to make them more managable.

---
+++ Content.Client/_EE/UserInterface/Controls/CodeEdit/CodeEdit.Types.LineNumberColumn.cs
@@ -0,0 +1,60 @@
+using System.Text;
+using Robust.Client.Graphics;
+using Robust.Client.UserInterface;
+using Robust.Shared.Utility;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Content.Client._EE.UserInterface.Controls.CodeEdit;
+
+public sealed partial class CodeEdit
+{
+    internal sealed partial class RenderBox
+    {
+        internal sealed partial class LineNumberColumn
+        {
+            protected override void Draw(DrawingHandleScreen handle)
+            {
+                // Draw line numbers with scrolling adjustment
+                var lineNumberFont = _master.GetFont();
+                var lineNumberColor = Color.Gray;
+                var lineNumberBaseLine = new Vector2(5, -_master._scrollBar.Value + lineNumberFont.GetAscent(UIScale));
+
+                void DrawNum(uint num, Vector2 pos)
+                {
+                    foreach (var r in num.ToString().EnumerateRunes())
+                        pos.X += lineNumberFont.DrawChar(handle, r, pos, UIScale, lineNumberColor);
+                }
+
+                var lineNumber = 1u;
+
+                var inWordWrap = false;
+
+                foreach (var breakIdx in _master._lineBreaks)
+                {
+                    // Only increment the line count if there was an explicit break; Word wrapping doesn't count.
+                    var lineBreak = Rope.Index(_master.TextRope, breakIdx) == '\n';
+                    // A num is drawn if there's a break, or if we're only *starting* a wrap, at the beginning.
+                    var drawNum = (lineBreak || !inWordWrap) && !inWordWrap;
+
+                    // Draw the number as long as we're not in a wrap.
+                    // We use a copy of the base line so we don't modify the original for next line.
+                    if (drawNum)
+                    {
+                        DrawNum(lineNumber, lineNumberBaseLine);
+                        lineNumber++; // Only increment if we drew a number.
+                    }
+
+                    // Move the cursor to the next line.
+                    lineNumberBaseLine.Y += _master.GetFont().GetLineHeight(UIScale);
+
+                    // In the case of a word wrap, make note we're currently in one so we know not to draw a line number until we're out.
+                    inWordWrap = !lineBreak;
+                }
+
+                // There might be one more line number to draw right at the end.
+                if (!inWordWrap)
+                    DrawNum(lineNumber, lineNumberBaseLine);
+            }
+        }
+    }
+}
--- Content.Client/_EE/UserInterface/Controls/CodeEdit/CodeEdit.Types.cs
+++ Content.Client/_EE/UserInterface/Controls/CodeEdit/CodeEdit.Types.cs
@@ -1,7 +1,7 @@
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Utility;
-using Vector2 = System.Numerics.Vector2;
 
 namespace Content.Client._EE.UserInterface.Controls.CodeEdit;
 
@@ -13,202 +13,46 @@ public sealed partial class CodeEdit
     /// <remarks>
     /// This is a sub-control to use <see cref="Control.RectClipContent"/>.
     /// </remarks>
-    internal sealed class RenderBox : Control
+    internal sealed partial class RenderBox : BoxContainer
     {
         private readonly CodeEdit _master;
+
+        private readonly LineNumberColumn _lineNumberColumn;
+        private readonly TextBox _textBox;
+
+        public StyleBox? LineColumnPanel { set => _lineNumberColumn.PanelOverride = value; get => _lineNumberColumn.PanelOverride; }
 
         public RenderBox(CodeEdit master)
         {
             _master = master;
 
             RectClipContent = true;
+            Orientation = LayoutOrientation.Horizontal;
+            AddChild(_lineNumberColumn = new LineNumberColumn(this));
+            AddChild(_textBox = new TextBox(this));
         }
 
-        protected override void Draw(DrawingHandleScreen handle)
+        internal sealed partial class TextBox : Control
         {
-            var drawBox = PixelSizeBox;
-            var font = _master.GetFont();
-            var renderedTextColor = _master.GetFontColor();
+            private readonly CodeEdit _master;
 
-            var scrollOffset = -_master._scrollBar.Value;
-
-            var scale = UIScale;
-            var baseLine = new Vector2(0, scrollOffset + font.GetAscent(scale));
-            var height = font.GetLineHeight(scale);
-            var descent = font.GetDescent(scale);
-
-            var viewT = -scrollOffset;
-
-            var startLineIndex = (int) (viewT / height);
-            var startIdx = _master.GetStartOfLine(startLineIndex);
-
-            var lineBreakIndex = startLineIndex;
-            var count = startIdx;
-
-            var selectionLower = _master.SelectionLower;
-            var selectionUpper = _master.SelectionUpper;
-
-            baseLine.Y += startLineIndex * height;
-
-            int? selectStartPos = null;
-            int? selectEndPos = null;
-            var selecting = false;
-
-            var imeStartIndex = -1;
-            var imeEndIndex = -1;
-
-            int? imeStartPos = null;
-            int? imeEndPos = null;
-            var imeing = false;
-
-            if (_master._imeData.HasValue)
+            public TextBox(RenderBox master)
             {
-                var (start, length) = _master._imeData.Value;
-                imeStartIndex = start.Index;
-                imeEndIndex = imeStartIndex + length;
-
-                if (imeStartIndex < startIdx && imeEndIndex > startIdx)
-                {
-                    imeing = true;
-                    imeStartPos = 0;
-                }
+                _master = master._master;
+                HorizontalExpand = true;
+                RectClipContent = true;
             }
+        }
 
-            if (selectionLower.Index < startIdx && selectionUpper.Index > startIdx)
+        internal sealed partial class LineNumberColumn : PanelContainer
+        {
+            private readonly CodeEdit _master;
+
+            public LineNumberColumn(RenderBox master)
             {
-                selecting = true;
-                selectStartPos = 0;
-            }
-
-            foreach (var rune in Rope.EnumerateRunes(_master.GetDisplayRope(), startIdx))
-            {
-                CheckDrawCursors(LineBreakBias.Top);
-
-                if (lineBreakIndex < _master._lineBreaks.Count
-                    && _master._lineBreaks[lineBreakIndex] == count)
-                {
-                    // Line break
-                    // Check to handle
-
-                    PostDrawLine();
-
-                    baseLine = new Vector2(drawBox.Left, baseLine.Y + height);
-                    lineBreakIndex += 1;
-
-                    selectStartPos = selecting ? 0 : null;
-                    selectEndPos = null;
-
-                    imeStartPos = imeing ? 0 : null;
-                    imeEndPos = null;
-
-                    if (baseLine.Y - height > drawBox.Height)
-                    {
-                        // Past the bottom of the visible area of the screen: no need to render anything else.
-                        break;
-                    }
-                }
-
-                CheckDrawCursors(LineBreakBias.Bottom);
-
-                baseLine.X += font.DrawChar(handle, rune, baseLine, scale, renderedTextColor);
-
-                count += rune.Utf16SequenceLength;
-            }
-
-            // Also draw cursor if it's at the very end.
-            CheckDrawCursors(LineBreakBias.Bottom);
-            CheckDrawCursors(LineBreakBias.Top);
-            PostDrawLine();
-
-            void CheckDrawCursors(LineBreakBias bias)
-            {
-                var pos = new CursorPos(count, bias);
-
-                if (_master.HasKeyboardFocus() && _master._cursorPosition == pos)
-                {
-                    var cursorColor = _master.StylePropertyDefault(
-                        StylePropertyCursorColor,
-                        Color.White);
-
-                    cursorColor.A *= _master._blink.Opacity;
-
-                    handle.DrawRect(
-                        new UIBox2(
-                            baseLine.X,
-                            baseLine.Y - height + descent,
-                            baseLine.X + 1,
-                            baseLine.Y + descent),
-                        cursorColor);
-
-                    if (UserInterfaceManager.KeyboardFocused == _master && Root?.Window is { } window)
-                    {
-                        var box = (UIBox2i) new UIBox2(
-                            drawBox.Left,
-                            baseLine.Y - height + descent,
-                            drawBox.Right,
-                            baseLine.Y + descent);
-                        var cursorOffset = baseLine.X - drawBox.Left;
-
-                        window.TextInputSetRect(box.Translated(GlobalPixelPosition), (int) cursorOffset);
-                    }
-                }
-
-                if (selectionLower == pos)
-                {
-                    selecting = true;
-                    selectStartPos = (int) baseLine.X;
-                }
-
-                if (selectionUpper == pos)
-                {
-                    selecting = false;
-                    selectEndPos = (int) baseLine.X;
-                }
-
-                if (count == imeStartIndex)
-                {
-                    imeing = true;
-                    imeStartPos = (int) baseLine.X;
-                }
-
-                if (count == imeEndIndex)
-                {
-                    imeing = false;
-                    imeEndPos = (int) baseLine.X;
-                }
-            }
-
-            void PostDrawLine()
-            {
-                if (selectStartPos != null)
-                {
-                    var rect = new UIBox2(
-                        selectStartPos.Value,
-                        baseLine.Y - height + descent,
-                        selectEndPos ?? baseLine.X,
-                        baseLine.Y + descent
-                    );
-
-                    var color = _master.StylePropertyDefault(
-                        StylePropertySelectionColor,
-                        Color.CornflowerBlue.WithAlpha(0.25f));
-
-                    handle.DrawRect(rect, color);
-                }
-
-                if (_master._imeData.HasValue && imeStartPos.HasValue)
-                {
-                    // Draw IME underline.
-                    var y = baseLine.Y + font.GetDescent(scale);
-                    var rect = new UIBox2(
-                        imeStartPos.Value,
-                        y - 1,
-                        imeEndPos ?? baseLine.X,
-                        y
-                    );
-
-                    handle.DrawRect(rect, renderedTextColor);
-                }
+                _master = master._master;
+                SetWidth = (master._master.GetFont().GetCharMetrics(new('0'), UIScale)?.Width ?? 13) * 3;
+                RectClipContent = true;
             }
         }
     }
